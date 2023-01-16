39d69eae63a2df54c1f1d99d1cafcd66c3af0f59
Vision <visiondaniels32@gmail.com>
2023-01-16 14:55:13 +0000

    Updated scrolling issue

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import { createSignal, For } from 'solid-js';
+import { createEffect, createSignal, For, on } from 'solid-js';
 import styles from '../App.module.css';
 import useFirestore from '../hooks/useFirestore';
 import { app } from '..';
@@ -10,6 +10,7 @@ export const Chat = () => {
   const data = getData('chats');
   const authState = useAuth(app);
   let submit_input: any;
+  let container: any;
 
   function handleSubmit() {
     addData('chats', {
@@ -18,10 +19,16 @@ export const Chat = () => {
     submit_input.value = '';
   }
 
+  createEffect(
+    on(data, () => {
+      container.scrollTop = container.scrollHeight;
+    })
+  );
+
   return (
     <div class={styles.App}>
       <h1>Message the Group</h1>
-      <div class={styles.App__messages}>
+      <div class={styles.App__messages} ref={container}>
         <For each={data()}>
           {(message) => (
             <div
